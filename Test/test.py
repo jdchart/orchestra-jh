31ab985721f3802ffd5989387b02ff74dc1f6f24
jdchart <jacob.dchart@gmail.com>
2023-06-16 15:14:10 +0200

    Test script

---
+++ Test/test.py
@@ -0,0 +1,7 @@
+import orchestrajh as oc
+
+oc.process(
+    "/Users/jacob/Documents/Git Repos/orchestra-jh/Test/Input",
+    "/Users/jacob/Documents/Git Repos/orchestra-jh/Test/Output",
+    filename = "/Users/jacob/Documents/Git Repos/orchestra-jh/model/model.sav"
+)
